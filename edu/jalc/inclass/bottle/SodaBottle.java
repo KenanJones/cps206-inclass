8d82f49f90209bb457949d3efc3c3d403a42cb54
E231 <E231@volnet.jalcdom.jal.cc.il.us>
2017-02-09 08:47:11 -0600

    add tests

--- edu/jalc/inclass/bottle/SodaBottle.java
+++ edu/jalc/inclass/bottle/SodaBottle.java
@@ -1,25 +1,38 @@
 package edu.jalc.inclass.bottle;
 
-import edu.jalc.inclass.lid.*;
+import edu.jalc.inclass.lid.SodaBottleLid;
+import edu.jalc.inclass.liquid.Soda;
 
 public class SodaBottle {
-	//final only after the constructor finishes
+
 	private final double capacity;
-	private SodaBottleLid lid = new SodaBottleLid(30,true);
+	private SodaBottleLid lid;
+	private Soda soda;
 
-	private SodaBottle(){
+	private SodaBottle() throws Exception {
 		this.capacity = 0;
+		this.setLid(null);
+		this.soda = null;
 	}
 
-	SodaBottle(double capacity){
+	SodaBottle(double capacity, SodaBottleLid lid, Soda soda) throws Exception {
 		this.capacity = capacity;
+		this.setLid(lid);
+		this.setSoda(soda);
 	}
 
-	double getCapacity(){
-		return capacity;
+	double getCapacity(){ return capacity; }
+	SodaBottleLid getLid(){	return this.lid; }
+	Soda getSoda(){ return this.soda; }
+
+	SodaBottle setLid(SodaBottleLid lid) throws Exception {
+		if(lid == null) throw new Exception("SodaBottleLid cannot be null");
+		this.lid = lid;
+		return this;
 	}
 
-	public SodaBottleLid getLid(){
-		return lid;
+	SodaBottle setSoda(Soda soda){
+		this.soda = soda;
+		return this;
 	}
 }
--- edu/jalc/inclass/lid/SodaBottleLid.java
+++ edu/jalc/inclass/lid/SodaBottleLid.java
@@ -1,29 +1,37 @@
 package edu.jalc.inclass.lid;
 
-public class SodaBottleLid{
+public class SodaBottleLid {
 
-  private final double height;
-  private boolean attached;
+	private boolean attached;
+	private final double height;
+	private final double radius;
 
-  private SodaBottleLid(){
-    height = 0;
-    setAttached(false);
-  }
+	private SodaBottleLid(){
+		this.attached = false;
+		this.height = this.radius = 0;
+	}
 
-  public SodaBottleLid(double height, boolean attached){
-    this.height = height;
-    setAttached(attached);
-  }
+	public SodaBottleLid(double height, double radius){
+		this.height = height;
+		this.radius = radius;
+		this.attached = false;
+	}
 
-  public void setAttached(boolean attached){
-    this.attached = attached;
-  }
+	public double getHeight(){ return this.height; }
+	public double getRadius(){ return this.radius; }
+	public boolean isAttached(){ return this.attached; }
 
-  public void attach(){
-    setAttached(true);
-  }
+	public SodaBottleLid attach(){
+		this.setAttached(true);
+		return this;
+	}
 
-  public void detach(){
-    setAttached(false);
-  }
+	public SodaBottleLid detach(){
+		this.setAttached(false);
+		return this;
+	}
+
+	private void setAttached(boolean attached){
+		this.attached = attached;
+	}
 }
